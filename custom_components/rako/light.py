c95987c91e878650fa8d77fa32b6a0d7c45ff86d
boshjosh1918 <67383578+boshjosh1918@users.noreply.github.com>
2022-10-22 01:16:54 +0100

    Update light.py

---
+++ custom_components/rako/light.py
@@ -0,0 +1,6 @@
+"""Light platform for rako."""
+
+from homeassistant.components.light import LightEntity
+
+from .const import DEFAULT_NAME, DOMAIN, ICON, SWITCH
+from .entity import IntegrationBlueprintEntity
